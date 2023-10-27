9b06dbd3ca44f7ab39ce703785f5d8612819d4f7
irecalfer <irene.c.f@outlook.es>
2023-10-27 07:11:11 +0200

    Añadidas guias a activity_home.xml.

--- app/src/main/java/com/calferinnovate/mediconnecta/Home/HomeActivity.java
+++ app/src/main/java/com/calferinnovate/mediconnecta/Home/HomeActivity.java
@@ -13,29 +13,14 @@ import com.calferinnovate.mediconnecta.clases.Unidades;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Empleado empleado;
-    Unidades unidad;
-    TextView empeladoNombre;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        empeladoNombre = (TextView) findViewById(R.id.prueba);
-
-        llamaAClaseGlobal();
-        pintaNombreEmpleado(unidad);
 
     }
 
-    public Unidades llamaAClaseGlobal(){
-        unidad = ((ClaseGlobal) getApplicationContext()).unidades;
-        return unidad;
-    }
-
-    public void pintaNombreEmpleado(Unidades e){
-        empeladoNombre.setText(unidad.getNombreUnidad());
-    }
 
 }
