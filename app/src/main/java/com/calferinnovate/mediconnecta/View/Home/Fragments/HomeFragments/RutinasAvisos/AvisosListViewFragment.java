573bbca4706daddaf7804a59522f76c74c3b4ded
irecalfer <irene.c.f@outlook.es>
2023-11-26 17:15:00 +0100

    Documentado AvisosListViewFragment

--- app/src/main/java/com/calferinnovate/mediconnecta/View/Home/Fragments/HomeFragments/RutinasAvisos/AvisosListViewFragment.java
+++ app/src/main/java/com/calferinnovate/mediconnecta/View/Home/Fragments/HomeFragments/RutinasAvisos/AvisosListViewFragment.java
@@ -22,8 +22,10 @@ import com.calferinnovate.mediconnecta.ViewModel.ViewModelFactory;
 import java.util.ArrayList;
 
 
+/**
+ * Fragmento que muestra la lista de avisos para una fecha dada
+ */
 public class AvisosListViewFragment extends Fragment {
-
 
     private ListView avisosLV;
     private String fecha;
@@ -34,23 +36,40 @@ public class AvisosListViewFragment extends Fragment {
     private ArrayAdapter<String> avisosAdapter;
 
 
+    /**
+     *  Método llamado cuando se crea la vista del fragmento.
+     *  Infla el diseño de la UI desde el archivo XML fragment_avisos_list_view.xml.
+     *
+     * @param inflater           Inflador utilizado para inflar el diseño de la UI.
+     * @param container          Contenedor que contiene la vista del fragmento
+     * @param savedInstanceState Estado de guardado de la instancia del fragmento
+     * @return Es la vista inflada del fragmento.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View vista = inflater.inflate(R.layout.fragment_avisos_list_view, container, false);
-        //Declaramos los objetos globales y rellenaremos el ListView
+
         inicializaVariables(vista);
         inicializaViewModel();
         obtieneFecha();
-        Log.d("aviso", "HA llegado aquí");
+
         return vista;
     }
 
+    /**
+     * Enlaza los recursos de la UI con nuestras variables.
+     *
+     * @param view La vista inflada.
+     */
     private void inicializaVariables(View view) {
         avisosLV = view.findViewById(R.id.listViewAvisos);
     }
 
+    /**
+     * Método que configura el ViewModel AvisosViewModel mediante la creación de un ViewModelFactory
+     * que proporciona una instancia de Peticiones Json al ViewModel.
+     */
     private void inicializaViewModel(){
         viewModelArgs = () -> peticionesJson = new PeticionesJson(requireContext());
 
@@ -59,6 +78,9 @@ public class AvisosListViewFragment extends Fragment {
         avisosViewModel = new ViewModelProvider(requireActivity(), factory).get(AvisosViewModel.class);
     }
 
+    /**
+     * Obtiene la fecha de los argumentos del fragmento y llama a rellenaListView si la fecha es válida.
+     */
     public void obtieneFecha(){
         Bundle bundle = getArguments();
         if(bundle != null){
@@ -71,15 +93,10 @@ public class AvisosListViewFragment extends Fragment {
     }
 
 
-    //Accederemos a nuestro avisos.php e iremos cogiendo cada fila y la guardaremos en el objeto avisos
-    //Guardaremos el contenido de los mensajes en una lista, ya que es lo único que deseamos que se
-    //muestre en el listView.
-    //A continuación llamamos a nuestro adaptador, donde le pasaremos el layout donde se encuentra el TextView
-    //que será lo que insertemos en el list View, pasamos también el id del TextView y por último la lista.
-    //Seteamos nuestro adaptador en el ListView
-    //Y añadiamos a la cola nuestro json request.
-    //Tras terminar podremos ver como en HomeFragment se encuentra el lisview completado con los avisos
-    //correspondientes a la fecha elegida.
+    /**
+     * Método que llama al método obtieneAvisosFecha del AvisosViewModel y le pasa la fecha seleccionada.
+     * Obtiene una lista con el contenido de los avisos y actualiza el ListView con un ArrayAdapter.
+     */
     public void rellenaListView(){
         avisosViewModel.obtieneAvisosFecha(fecha).observe(getViewLifecycleOwner(), new Observer<ArrayList<String>>() {
             @Override
