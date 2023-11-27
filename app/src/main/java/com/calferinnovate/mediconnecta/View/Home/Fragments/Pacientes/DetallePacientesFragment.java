7a6ccfa41275005f2dfb6f4460641caca1e57863
irecalfer <irene.c.f@outlook.es>
2023-11-27 11:49:41 +0100

    JavaDoc DetallePacientesFragment

--- app/src/main/java/com/calferinnovate/mediconnecta/View/Home/Fragments/Pacientes/DetallePacientesFragment.java
+++ app/src/main/java/com/calferinnovate/mediconnecta/View/Home/Fragments/Pacientes/DetallePacientesFragment.java
@@ -6,32 +6,42 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainerView;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.calferinnovate.mediconnecta.View.Home.Fragments.PacientesFragment;
 import com.calferinnovate.mediconnecta.View.IOnBackPressed;
 import com.calferinnovate.mediconnecta.Model.Area;
 import com.calferinnovate.mediconnecta.Model.ClaseGlobal;
-import com.calferinnovate.mediconnecta.Model.Pacientes;
 import com.calferinnovate.mediconnecta.Model.Unidades;
 import com.calferinnovate.mediconnecta.R;
 import com.calferinnovate.mediconnecta.ViewModel.SharedPacientesViewModel;
 import com.google.android.material.tabs.TabLayout;
 
+/**
+ * Fragmento que proporciona un TabLayout para desplazarse por los datos del paciente y un fragment container
+ * donde se reemplazará los fragmentos de detalle del paciente.
+ */
 public class DetallePacientesFragment extends Fragment implements IOnBackPressed {
 
     private TabLayout tabLayoutPaciente;
-    private FragmentContainerView vistasDetallePaciente;
     private SharedPacientesViewModel sharedPacientesViewModel;
     private ClaseGlobal claseGlobal;
     private String nombreArea;
 
+    /**
+     * Método llamado cuando se crea la vista del fragmento.
+     * Infla el diseño de la UI desde el archivo XML fragment_detalle_pacientes.xml.
+     * Establece el título del Fragmento y configura el ViewModel.
+     *
+     * @param inflater inflador utilizado para inflar el diseño de la UI.
+     * @param container Contenedor que contiene la vista del fragmento
+     * @param savedInstanceState Estado de guardado de la instancia del fragmento
+     *
+     * @return vista Es la vista inflada del fragmento.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_detalle_pacientes, container, false);
         asignarValoresAVariables(view);
         sharedPacientesViewModel = new ViewModelProvider(requireActivity()).get(SharedPacientesViewModel.class);
@@ -42,16 +52,21 @@ public class DetallePacientesFragment extends Fragment implements IOnBackPressed
     }
 
 
+    /**
+     * Método que inicializa las variables miembro y enlaza con los recursos de la UI.
+     * @param view La vista inflada.
+     */
     public void asignarValoresAVariables(View view) {
         claseGlobal = ClaseGlobal.getInstance();
         tabLayoutPaciente = view.findViewById(R.id.tabLayoutDetallePacientes);
-        vistasDetallePaciente = view.findViewById(R.id.fragmentContainerDetallePacientes);
     }
 
 
+    /**
+     * Método que implementa la escucha de los Tabs del TabLayout, por defecto muestra GeneralPacientesFragment
+     * si un tab es seleccionado navega hasta dicho fragmento.
+     */
     public void listenerTabLayoutDetalle() {
-
-        // Inicialmente, obtén los datos para el tab actual
         if (tabLayoutPaciente.getTabCount() > 0) {
             getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerDetallePacientes, new GeneralPacientesFragment()).commit();
         }
@@ -60,6 +75,7 @@ public class DetallePacientesFragment extends Fragment implements IOnBackPressed
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 String tabSeleccionado = (String) tab.getText();
+                assert tabSeleccionado != null;
                 navegaAlFragmento(tabSeleccionado);
             }
 
@@ -77,32 +93,45 @@ public class DetallePacientesFragment extends Fragment implements IOnBackPressed
 
     }
 
+    /**
+     * Método que navega al fragmento según el Tab seleccionado. En caso de ser seleccionado el Tab Pautas
+     * llama a seleccionarTipoPautasArea().
+     * @param tabSeleccionado
+     */
     public void navegaAlFragmento(String tabSeleccionado){
-        if (tabSeleccionado.equals("General")) {
-            getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerDetallePacientes, new GeneralPacientesFragment()).commit();
-        } else if (tabSeleccionado.equals("Contactos")) {
-            getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerDetallePacientes, new ContactoFamiliaresPacienteFragment()).commit();
-        } else if (tabSeleccionado.equals("Clínica")) {
-            getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerDetallePacientes, new ClinicaPacientesFragment()).commit();
-        } else if (tabSeleccionado.equals("Pautas")) {
-            seleccionarTipoPautasAreas();
-        } else if (tabSeleccionado.equals("Parte")) {
-            getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerDetallePacientes, new PartePacientesFragment()).commit();
-        } else if (tabSeleccionado.equals("Parte Caídas")) {
-            getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerDetallePacientes, new ParteCaidasPacientesFragment()).commit();
+        switch (tabSeleccionado) {
+            case "General":
+                getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerDetallePacientes, new GeneralPacientesFragment()).commit();
+                break;
+            case "Contactos":
+                getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerDetallePacientes, new ContactoFamiliaresPacienteFragment()).commit();
+                break;
+            case "Clínica":
+                getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerDetallePacientes, new ClinicaPacientesFragment()).commit();
+                break;
+            case "Pautas":
+                seleccionarTipoPautasAreas();
+                break;
+            case "Parte":
+                getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerDetallePacientes, new PartePacientesFragment()).commit();
+                break;
+            case "Parte Caídas":
+                getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerDetallePacientes, new ParteCaidasPacientesFragment()).commit();
+                break;
         }
     }
 
+    /**
+     * Método que obtiene el Paciente seleccionado y verifica si pertenece al area de Geriatría o Salud Mental,
+     * según cual sea navega a un fragmento de Pautas diferente.
+     */
     public void seleccionarTipoPautasAreas() {
-        sharedPacientesViewModel.getPaciente().observe(getViewLifecycleOwner(), new Observer<Pacientes>() {
-            @Override
-            public void onChanged(Pacientes pacientes) {
-                for (Unidades unidades : claseGlobal.getListaUnidades()) {
-                    if (unidades.getId_unidad() == pacientes.getFkIdUnidad()) {
-                        for (Area areas : claseGlobal.getListaAreas()) {
-                            if (unidades.getFk_area() == areas.getId_area()) {
-                                nombreArea = areas.getNombre();
-                            }
+        sharedPacientesViewModel.getPaciente().observe(getViewLifecycleOwner(), pacientes -> {
+            for (Unidades unidades : claseGlobal.getListaUnidades()) {
+                if (unidades.getId_unidad() == pacientes.getFkIdUnidad()) {
+                    for (Area areas : claseGlobal.getListaAreas()) {
+                        if (unidades.getFk_area() == areas.getId_area()) {
+                            nombreArea = areas.getNombre();
                         }
                     }
                 }
@@ -115,6 +144,11 @@ public class DetallePacientesFragment extends Fragment implements IOnBackPressed
         }
     }
 
+    /**
+     * Método que agrega la lógica específica del fragmento para manejar el restroceso.
+     * Al presionar back volvería al PacientesFragment.
+     * @return true si el fragmento manejar el retroceso, false en caso contrario.
+     */
     @Override
     public boolean onBackPressed() {
         requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PacientesFragment()).commit();
