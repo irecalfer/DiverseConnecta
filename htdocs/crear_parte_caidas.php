7d90c515ca644cc1bbb0ea2df504210b6dccf986
Irene Caldelas Fernández <122487131+irecalfer@users.noreply.github.com>
2023-12-05 06:10:21 +0100

    Scripts PHP para la utilización de la librería Volley

---
+++ htdocs/crear_parte_caidas.php
@@ -0,0 +1,41 @@
+<?php
+
+    include 'conexion.php';
+    
+    //Recibe los parámetro de la app
+    $fecha_y_hora=$_POST["fecha_y_hora"];
+    $fk_cip_sns_paciente=$_POST["fk_cip_sns_paciente"];
+    $lugar_caida=$_POST["lugar_caida"];
+    $factores_de_riesgo=$_POST["factores_de_riesgo"];
+    $causas=$_POST["causas"];
+    $circunstancias=$_POST["circunstancias"];
+    $consecuencias=$_POST["consecuencias"];
+    $unidad=$_POST["unidad"];
+    $caida_presenciada=$_POST["caida_presenciada"];
+    $avisado_a_familiares=$_POST["avisado_a_familiares"];
+    $observaciones=$_POST["observaciones"];
+    $fk_cod_empleado=$_POST["fk_cod_empleado"];
+
+    $consulta = "INSERT INTO PARTE_DE_CAIDAS(fecha_y_hora, fk_cip_sns_paciente, lugar_caida, factores_de_riesgo, causas, circunstancias, consecuencias, unidad, caida_presenciada, avisado_a_familiares, observaciones, fk_cod_empleado)
+                VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+
+    // Prepara la consulta
+    $stmt = $conexion->prepare($consulta);
+
+    // Vincula los parámetros
+    $stmt->bind_param("ssssssssssss", $fecha_y_hora, $fk_cip_sns_paciente, $lugar_caida, $factores_de_riesgo, $causas, $circunstancias, $consecuencias, $unidad, $caida_presenciada, $avisado_a_familiares, $observaciones, $fk_cod_empleado);
+
+    // Ejecuta la consulta
+if ($stmt->execute()) {
+    // Éxito, se registró el parte
+    $response = array('message' => 'Registro exitoso');
+    http_response_code(200); // Éxito
+} else {
+    // Error al registrar el parte
+    $response = array('message' => 'Error al registrar la caída');
+    http_response_code(500); // Error
+}
+
+// Devuelve la respuesta en formato JSON
+echo json_encode($response);
+?>
---
+++ htdocs/pacientes.php
@@ -0,0 +1,44 @@
+<?php
+
+    include 'conexion.php';
+
+if(isset($_GET["nombre"])){
+    $consulta = "SELECT P.*
+                 FROM PACIENTES P
+                 INNER JOIN UNIDADES U ON P.fk_id_unidad = U.id_unidad
+                 WHERE U.nombre='$_GET[nombre]'";
+
+
+    $resultado = $conexion ->query($consulta);
+    
+if($resultado -> num_rows > 0){
+    $return_arr['pacientes'] = array();
+    while($fila = $resultado->fetch_array()){
+        array_push($return_arr['pacientes'], array(
+        'cip_sns'=> $fila['cip_sns'],
+'nombre'=> $fila['nombre'],
+'apellidos'=> $fila['apellidos'],
+'foto'=> $fila['foto'],
+'num_seguridad_social'=> $fila['num_seguridad_social'],
+'fecha_nacimiento'=> $fila['fecha_nacimiento'],
+'dni'=> $fila['dni'],
+'estado_civil'=> $fila['estado_civil'],
+'lugar_nacimiento'=> $fila['lugar_nacimiento'],
+'sexo'=> $fila['sexo'],
+'fk_id_unidad'=> $fila['fk_id_unidad'],
+'fk_id_seguro'=> $fila['fk_id_seguro'],
+'fk_num_habitacion'=> $fila['fk_num_habitacion'],
+'fk_num_historia_clinica'=> $fila['fk_num_historia_clinica'],
+'fecha_ingreso'=> $fila['fecha_ingreso'],
+'estado_civil'=> $fila['estado_civil']
+        
+        
+       ));
+    }
+    echo json_encode($return_arr);
+}
+
+
+    $resultado -> close();
+}
+?>
